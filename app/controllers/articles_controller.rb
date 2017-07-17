5ef197a061668a0870fc2c5ab3a1ca1899b4f4a9
codeaj <codea_mac_05@CodeaCamp05s-Mac-mini.local>
2017-07-17 15:46:40 -0500

    añadiendo a lista blanca los parametros

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,21 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    #we need to change the create action to use the new Article model to save the data in the database
+    #every Rails model can be initialized with its respective attributes, which are automatically mapped to the respective database columns
+    @article = Article.new(article_params) #=> necesario  permitir los parametro, solucionado con el metodo article_params
+
+
+    @article.save
+    redirect_to @article
+
     # obtner hash con los parametro de l x a forma en la vista new.html.erb
-    render plain: params[:article].inspect
+    #render plain: params[:article].inspect
+  end
+
+  private
+  ##We have to whitelist our controller parameters to prevent wrongful mass assignment. In this case, we want to both allow and require the title and text parameters for valid use of create
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
